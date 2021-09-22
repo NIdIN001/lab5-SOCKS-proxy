221785c9ba32b120a55f614be980c23ecad2ba6d
Utochkin Stas <s.utochkin@g.nsu.ru>
2021-09-22 23:38:21 +0700

    не написал рукопожатие клиента и сервера

--- src/main/java/proxy/ProxyServer.java
+++ src/main/java/proxy/ProxyServer.java
@@ -2,14 +2,17 @@ package proxy;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Set;
 
 public class ProxyServer {
+    private final int BufferSize = 100;
     private int srcPort;
 
     private ServerSocketChannel serverSocket;
@@ -43,16 +46,56 @@ public class ProxyServer {
             while (iter.hasNext()) {
                 SelectionKey key = iter.next();
                 if (key.isAcceptable()) {
-                    //todo acceptConnection
+                    acceptConnection();
                 }
 
                 if (key.isReadable()) {
-                    // todo resend data
+                    resendData();
                 }
                 iter.remove();
             }
-
-
         }
+    }
+
+    private void acceptConnection() throws IOException {
+        SocketChannel client = serverSocket.accept();
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ);
+
+        ByteBuffer recvBuffer = ByteBuffer.allocate(BufferSize);
+        ByteBuffer sendBuffer = ByteBuffer.allocate(BufferSize);
+        client.read(recvBuffer);
+        if (recvBuffer.get(0) != 0x05 | recvBuffer.get(2) != 0x00) { // SOCKS version (0x05)
+            System.out.println("bad handshake by user");
+            client.close();
+        }
+        recvBuffer.clear();
+
+        sendBuffer.put(0, (byte) 0x05);
+        sendBuffer.put(1, (byte) 0x00);
+        client.write(sendBuffer);
+        sendBuffer.clear();
+
+        client.read(recvBuffer);
+        if (recvBuffer.get(0) != 0x05 | recvBuffer.get(1) != 0x01 | recvBuffer.get(2) != 0x00) {
+            System.out.println("bad handshake by user");
+            client.close();
+        }
+
+        byte[] destIP = new byte[4];
+        byte[] destPort = new byte[2];
+        System.arraycopy(recvBuffer.array(), 3, destIP, 0, 4);
+        System.arraycopy(recvBuffer.array(), 7, destPort, 0, 2);
+        recvBuffer.clear();
+
+        sendBuffer.put(0, (byte) 0x05);
+        sendBuffer.put(1, (byte) 0x00);
+        sendBuffer.put(2, (byte) 0x00);
+        //todo доделать https://en.wikipedia.org/wiki/SOCKS
+
+    }
+
+    private void resendData() {
+
     }
 }
